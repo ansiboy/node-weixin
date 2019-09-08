692bc66a681095a23265589bde4d271a035c38c0
maishu <ansiboy@163.com>
2019-09-08 23:56:23 +0800

    update

---
+++ src/common.ts
@@ -0,0 +1,12 @@
+import { WeiXinSDK } from "./lib/wexin-sdk";
+import { config } from "./config";
+
+export let wx = new WeiXinSDK();
+
+let w = config.weixin;
+wx.config.isSandBox = true;
+wx.config.appid = w.appId;
+wx.config.appkey = w.appKey;
+wx.config.partnerId = w.partnerId;
+wx.config.partnerKey = w.partnerKey;
+wx.config.isSandBox = true;
--- src/controllers/pay.ts
+++ src/controllers/pay.ts
@@ -1,18 +1,10 @@
-import { action } from "maishu-node-mvc";
-import { WeiXinSDK } from "../lib/wexin-sdk";
-import { config } from "../config";
+import { action, controller } from "maishu-node-mvc";
+import { wx } from "../common";
 
+@controller("pay")
 export class PayController {
     @action()
     async prepayid() {
-        let wx = new WeiXinSDK();
-        let w = config.weixin;
-
-        wx.config.isSandBox = true;
-        wx.config.partnerId = w.partnerId;
-        wx.config.partnerKey = w.partnerKey;
-        wx.config.isSandBox = true;
-
         let r = await wx.mch.unifiedorder({
             openid: "oYHEKuMV8Kt0QLBIMjmZfxoWwsjU", body: "body", notify_url: "www.163.com",
             out_trade_no: "111111", total_fee: "101"
